72bd18fbf7cb0af6a0cb0b4c4249f9dbda7b10b6
BugHunter-X <askzenai@gmail.com>
2025-02-19 14:14:09 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, SafeAreaView, ScrollView, Image } from 'react-native';
+
+const data = [
+  {
+    id: 1,
+    name: 'Item 1',
+    image: require('./images/image1.jpg')
+  },
+  {
+    id: 2,
+    name: 'Item 2',
+    image: require('./images/image2.jpg')
+  }
+];
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        {data.map((item) => (
+          <View key={item.id} style={styles.itemContainer}>
+            <Image source={item.image} style={styles.image} />
+            <Text style={styles.itemName}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  image: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  itemName: {
+    fontSize: 16,
+  },
+});
